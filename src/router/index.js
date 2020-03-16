0e5318d66f1eb90e7abac75d0eacff7e2081f54e
dlinda <linda.debray@outlook.com>
2020-03-16 23:25:22 +0100

    create 3 new components and set up router client side navogation

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,26 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventCreate from '../views/EventCreate.vue'
+import EventList from '../views/EventList.vue'
+import EventShow from '../views/EventShow.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
-  routes
-});
-
-export default router;
+export default new VueRouter({
+  routes: [{
+      path: '/',
+      name: 'event-list',
+      component: EventList
+    },
+    {
+      path: '/event',
+      name: 'event-show',
+      component: EventShow
+    },
+    {
+      path: '/event/create',
+      name: 'event-create',
+      component: EventCreate
+    }
+  ]
+})
