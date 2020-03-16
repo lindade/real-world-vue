b09ce93866ec3b35a92a68b0b601526a23c54f88
dlinda <linda.debray@outlook.com>
2020-03-17 00:27:18 +0100

    nested components added

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,15 @@
 <template>
-  <dir>
-    <h1>Event List</h1>
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }">Show Event #1</router-link>|
-  </dir>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard />
+  </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
